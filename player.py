12cc28655d0227b85d63a233f62e4f9b47d40ff5
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2024-11-21 19:51:14 +0100

    add: alpha-beta prunning

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import random
-
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
@@ -10,35 +9,27 @@ class PlayerControllerHuman(PlayerController):
     def player_loop(self):
         """
         Function that generates the loop of the game. In each iteration
-        the human plays through the keyboard and send
+        the human plays through the keyboard and sends
         this to the game through the sender. Then it receives an
         update of the game through receiver, with this it computes the
         next movement.
-        :return:
         """
-
         while True:
-            # send message to game that you are ready
             msg = self.receiver()
             if msg["game_over"]:
                 return
 
 
 class PlayerControllerMinimax(PlayerController):
-
     def __init__(self):
         super(PlayerControllerMinimax, self).__init__()
 
     def player_loop(self):
         """
         Main loop for the minimax next move search.
-        :return:
         """
-
-        # Generate game tree object
+        # Generate the first message (do not remove this line)
         first_msg = self.receiver()
-        # Initialize your minimax model
-        model = self.initialize_model(initial_data=first_msg)
 
         while True:
             msg = self.receiver()
@@ -46,49 +37,168 @@ class PlayerControllerMinimax(PlayerController):
             # Create the root node of the game tree
             node = Node(message=msg, player=0)
 
-            # Possible next moves: "stay", "left", "right", "up", "down"
-            best_move = self.search_best_next_move(
-                model=model, initial_tree_node=node)
+            # Use minimax with alpha-beta pruning to find the best move
+            best_move = self.search_best_next_move(initial_tree_node=node)
 
-            # Execute next action
+            # Send the best move to the game
             self.sender({"action": best_move, "search_time": None})
 
-    def initialize_model(self, initial_data):
+    def search_best_next_move(self, initial_tree_node):
         """
-        Initialize your minimax model 
-        :param initial_data: Game data for initializing minimax model
-        :type initial_data: dict
-        :return: Minimax model
-        :rtype: object
-
-        Sample initial data:
-        { 'fish0': {'score': 11, 'type': 3}, 
-          'fish1': {'score': 2, 'type': 1}, 
-          ...
-          'fish5': {'score': -10, 'type': 4},
-          'game_over': False }
-
-        Please note that the number of fishes and their types is not fixed between test cases.
-        """
-        # EDIT THIS METHOD TO RETURN A MINIMAX MODEL ###
-        return None
-
-    def search_best_next_move(self, model, initial_tree_node):
-        """
-        Use your minimax model to find best possible next move for player 0 (green boat)
-        :param model: Minimax model
-        :type model: object
-        :param initial_tree_node: Initial game tree node 
-        :type initial_tree_node: game_tree.Node 
-            (see the Node class in game_tree.py for more information!)
-        :return: either "stay", "left", "right", "up" or "down"
-        :rtype: str
+        Search for the best next move using Iterative Deepening Search (IDS) and the Minimax algorithm with alpha-beta pruning.
+        Args:
+            initial_tree_node (TreeNode): The initial node of the game tree from which to start the search.
+        Returns:
+            str: The best move as a string representation.
         """
 
-        # EDIT THIS METHOD TO RETURN BEST NEXT POSSIBLE MODE FROM MINIMAX MODEL ###
+        # Best move and score
+        best_move = -1
+        best_score = float("-inf")
+
+        # Iterative Deepening Search (IDS)
+        for depth in range(1, 20):
+
+            # Obtain the score of the node
+            score = self.minimax_alpha_beta_search(initial_tree_node, depth)
+
+            # Get the best move
+            if score > best_score:
+                best_score = score
+                best_move = initial_tree_node.move
+
+        return ACTION_TO_STR[best_move]
+
+    def minimax_alpha_beta_search(self, node, depth):
+        """
+        Perform the minimax search with alpha-beta pruning.
+        Args:
+            node: The current state of the game.
+            depth: The maximum depth to search in the game tree.
+        Returns:
+            The best score for the current player.
+        """
         
-        # NOTE: Don't forget to initialize the children of the current node 
-        #       with its compute_and_get_children() method!
+        # Alpha and Beta for pruning
+        alpha = float("-inf")
+        beta = float("inf")
 
-        random_move = random.randrange(5)
-        return ACTION_TO_STR[random_move]
+        # Best move and score
+        value = self.max_value(node, depth, alpha, beta)
+
+        return value
+        
+    def max_value(self, node, depth, alpha, beta):
+        """
+        Computes the maximum value for the given node using the minimax algorithm with alpha-beta pruning.
+        Args:
+            node (Node): The current node in the game tree.
+            depth (int): The current depth in the game tree.
+            alpha (float): The alpha value for alpha-beta pruning.
+            beta (float): The beta value for alpha-beta pruning.
+        Returns:
+            float: The maximum value computed for the given node.
+        """
+
+        # Terminal state
+        if depth == 0 or len(node.state.get_fish_positions()) == 0:
+            return self.heuristic(node)
+        
+        # value <- -∞
+        value = float("-inf")
+
+        # Move Ordering 
+        children = node.compute_and_get_children()
+        children.sort(key=lambda child: self.heuristic(child), reverse=True)
+
+        for child in children:
+            # Recursively call min_value to evaluate the child node
+            value = max(value, self.min_value(child, depth - 1, alpha, beta))
+            # If the value is greater than or equal to beta, prune the remaining branches
+            if value >= beta:
+                return value
+            # Update alpha with the maximum value
+            alpha = max(alpha, value)
+
+        return value
+
+    def min_value(self, node, depth, alpha, beta):
+        """
+        Computes the minimum value for the given node using the minimax algorithm with alpha-beta pruning.
+        Args:
+            node (Node): The current node in the game tree.
+            depth (int): The current depth in the game tree.
+            alpha (float): The alpha value for alpha-beta pruning.
+            beta (float): The beta value for alpha-beta pruning.
+        Returns:
+            float: The minimum value computed for the given node.
+        """
+
+        # Terminal state
+        if depth == 0 or len(node.state.get_fish_positions()) == 0:
+            return self.heuristic(node)
+        
+        # value <- +∞
+        value = float("inf")
+
+        # Move Ordering 
+        children = node.compute_and_get_children()
+        children.sort(key=lambda child: self.heuristic(child))
+
+        for child in children:
+            # Recursively call max_value to evaluate the child node
+            value = min(value, self.max_value(child, depth - 1, alpha, beta))
+            # If the value is less than or equal to alpha, prune the remaining branches
+            if value <= alpha:
+                return value
+            # Update beta with the minimum value
+            beta = min(beta, value)
+
+        return value
+
+    def heuristic(self, currentNode):
+        state = currentNode.state
+        score_player, score_opp = state.get_player_scores()
+
+        difference = score_player - score_opp
+
+        hook_positions = state.get_hook_positions()
+        hook_player = hook_positions[0]
+        hook_opp = hook_positions[1]
+
+        fish_positions = state.get_fish_positions()
+        fish_values = state.get_fish_scores()
+
+        for fish_number, fish_pos in fish_positions.items():
+            distance_player = self.calculate_distance(hook_player, fish_pos)#distance from player's hook to fish
+            distance_opp = self.calculate_distance(hook_opp, fish_pos)#distance from opponent's hook to fish
+
+            fish_value = fish_values[fish_number]
+
+            if distance_player == 0: #fish caught by player
+                difference += fish_value
+            elif distance_opp == 0: #fish caught by opponent
+                difference -= fish_value
+            else:
+                if fish_value > 0:
+                    difference += (fish_value /distance_player) + 3
+                    difference -= fish_value / distance_opp
+                else:
+                    difference -= abs(fish_value)/ distance_player
+                    difference += abs(fish_value)/ distance_opp
+            
+            if distance_player == distance_opp:
+                difference -= abs(fish_value) /distance_player
+
+        caught_fish = state.get_caught()
+        #adding the values of the fish caught to the difference in score
+        if caught_fish[0] is not None:
+            difference += fish_values[caught_fish[0]] 
+        if caught_fish[1] is not None:
+            difference -= fish_values[caught_fish[1]]
+
+        return difference #finally, the the score difference is returned, taking into consideration the heuristic value
+
+    #auxiliary method to calculate distance between two positions
+    def calculate_distance(self, pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
